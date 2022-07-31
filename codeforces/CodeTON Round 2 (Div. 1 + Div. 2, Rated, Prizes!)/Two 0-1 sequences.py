2a177b362a62403ef84d63bd13cc5344eb53a670
lakshayroop5 <lakshayroopchandani5@gmail.com>
2022-08-01 02:10:38 +0530

    Two 0-1 sequences

--- codeforces/CodeTON Round 2 (Div. 1 + Div. 2, Rated, Prizes!)/Two 0-1 sequences.py
+++ codeforces/CodeTON Round 2 (Div. 1 + Div. 2, Rated, Prizes!)/Two 0-1 sequences.py
@@ -4,39 +4,11 @@ for _ in range(T):
     n, m = map(int, input().split())
     a = input()
     b = input()
-    x = list(a)
-    o = []
 
-
-    def func(s: list, c):
-        # global m
-        # print(s)
-        if len(s) == m or len(s) < 2:
-            # print(s, 'a')
-            o.append(s)
-            return
-
-        if c == '1':
-            # print(s, 'n')
-            temp = min(int(s[-1]), int(s[-2]))
-            s.pop()
-            s.pop()
-            s += str(temp)
-
-        if c == '0':
-            # print(s, 'm')
-            temp = max(int(s[-1]), int(s[-2]))
-            s.pop()
-            s.pop()
-            s += str(temp)
-        func(s.copy(), '1')
-        # print('v', s)
-        func(s.copy(), '0')
-
-
-    func(x[::-1], '1')
-    func(x[::-1], '0')
-    if o.__contains__(list(b)[::-1]):
-        print('Yes')
-    else:
+    if b[1:] != a[len(a) - len(b) + 1:]:
         print('No')
+        continue
+    if not a[:len(a) - len(b) + 1].__contains__(b[0]):
+        print('No')
+        continue
+    print('Yes')
